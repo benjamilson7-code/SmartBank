628fe3f7e809549db875fec30439547f9c7d40e0
Benjamim Kipopi Mukwalemba <a52969@alunos.ipb.pt>
2026-02-21 18:30:29 +0000

    atualizar readme

--- README.md
+++ README.md
@@ -1,1 +1,168 @@
 # Smart-Bank
+
+ SmartBank - Sistema ATM em Java
+ Sobre o Projeto
+
+O SmartBank é uma aplicação desenvolvida em Java que simula o funcionamento básico de um caixa eletrônico (ATM) através do terminal (console).
+
+O sistema permite realizar operações bancárias essenciais como criação de conta, depósito, saque e transferência entre contas.
+
+Este projeto foi desenvolvido com foco na aplicação de conceitos de Programação Orientada a Objetos (POO).
+
+#  Objetivo
+
+Aplicar conceitos fundamentais de:
+
+Programação Orientada a Objetos
+
+Organização em pacotes
+
+Estrutura de dados dinâmica (ArrayList)
+
+Tratamento de exceções
+
+Interação com o usuário via console
+
+#  Tecnologias Utilizadas
+
+Java
+
+ POO (Programação Orientada a Objetos)
+
+ArrayList
+
+Scanner (entrada de dados)
+
+Organização em pacotes
+
+#  Estrutura do Projeto
+src/
+│
+├── view/
+│   └── SmartBank.java
+│
+├── model/
+│   ├── Cliente.java
+│   └── Conta.java
+│
+└── Helper/
+    └── Utils.java
+ # Descrição das Classes
+## SmartBank.java
+
+Classe principal do sistema.
+
+Responsável por:
+
+Exibir o menu principal
+
+Controlar as operações
+
+Gerenciar a lista de contas
+
+Buscar contas pelo número
+
+Interagir com o usuário
+
+##   Cliente.java
+
+Classe que representa o cliente do banco.
+
+Contém informações como:
+
+Nome
+
+Email
+
+CPF
+
+Data de nascimento
+
+##   Conta.java
+
+Classe que representa a conta bancária.
+
+Funcionalidades:
+
+Depositar valores
+
+Sacar valores
+
+Transferir dinheiro entre contas
+
+Exibir dados da conta
+
+##   Utils.java
+
+Classe auxiliar com métodos utilitários:
+
+Conversão de String para Data
+
+Método de pausa do sistema
+
+Auxílio em formatações
+
+#  Funcionalidades do Sistema
+
+O sistema apresenta o seguinte menu:
+
+1 - Criar conta
+2 - Efetuar saque
+3 - Efetuar depósito
+4 - Efetuar transferência
+5 - Listar contas
+6 - Sair do sistema
+▶️ Como Executar o Projeto
+##   1️⃣ Clonar o repositório
+https://github.com/benjamilson7-code/SmartBank.git
+##  2️⃣ Abrir em uma IDE
+
+Pode ser executado em:
+
+IntelliJ IDEA
+
+Eclipse
+
+VS Code
+
+NetBeans
+
+ Executar a classe principal
+
+##  Execute o arquivo:
+
+  SmartBank.java
+
+# Conceitos de POO Aplicados
+
+Encapsulamento
+
+Associação entre classes
+
+Abstração
+
+Organização modular
+
+Manipulação de objetos
+
+Estrutura de dados dinâmica
+
+# Possíveis Melhorias Futuras
+
+Persistência de dados (Arquivo ou Banco de Dados)
+
+Interface gráfica (JavaFX ou Swing ou Angular)
+
+Sistema de autenticação
+
+Histórico de transações
+
+Validação avançada de dados
+
+Testes unitários
+
+# Autor
+
+Benjamim Kipopi Mukwalemba
+Estudante de Engenharia Informática
+Foco em Desenvolvimento Backend e Sistemas
--- src/view/SmartBank.java
+++ src/view/SmartBank.java
@@ -1,10 +1,208 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Helper.Utils;
+import model.Cliente;
+import model.Conta;
+
 public class SmartBank {
-
+	  static String Nome="Smart Bank";
+      static Scanner teclado= new Scanner(System.in);
+	  static ArrayList<Conta>contas;
+	  
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SmartBank.contas=new ArrayList<Conta>();
+		SmartBank.menu();
 
+	}
+
+	private static void menu() {
+		int opcao=0;
+		System.out.println("======================================");
+		System.out.println("================= ATM ================");
+		System.out.println("============= "+SmartBank.Nome+" =============");
+		System.out.println("======================================");
+		System.out.println(" Selecine uma das opções no menu");
+		System.out.println("1- Criar conta ");
+		System.out.println("2- Efetuar Saque");
+		System.out.println("3- Efetuar Deposito");
+		System.out.println("4- Efetuar Transferencia");
+		System.out.println("5- Listar Contas");
+		System.out.println("6- Sair do sistema ");
+		try {
+			opcao=Integer.parseInt(SmartBank.teclado.nextLine());
+		} catch (Exception e){
+			System.out.println(e);
+			Utils.pausar(3);
+			SmartBank.menu();
+		}
+		
+		switch (opcao) {
+		case 1:
+			SmartBank.CriarConta();
+			   break;
+		case 2:
+			SmartBank.EfetuarSaque();
+			   break;
+		case 3:
+			SmartBank.EfetuarDeposito();
+			   break;
+		case 4:
+			SmartBank.EfetuarTransferencia();
+			   break;
+		case 5:
+			SmartBank.ListarContas();
+			   break;
+		case 6:
+			   System.out.println("Ate a proxima");
+			   Utils.pausar(3);
+			   System.exit(0);
+		default:
+			System.out.println("Opção invalida");
+			Utils.pausar(3);
+			SmartBank.menu();
+			break;
+		}
+		
+	}
+
+	private static void ListarContas() {
+		
+		if(SmartBank.contas.size()>0) {
+			System.out.println("Listando As contas ...");
+			System.out.println(" ");
+			for(Conta cont: SmartBank.contas) {
+				System.out.println(cont);
+				System.out.println(" ");
+				Utils.pausar(1);
+			}
+			
+		}
+		else {
+			System.out.println("Nao existe contas Cadastradas");
+		}
+		Utils.pausar(5);
+		SmartBank.menu();
+		
+	}
+
+	private static void EfetuarTransferencia() {
+System.out.println("Efetuando Transferencia ...");
+		
+		System.out.println("Informe o numero da sua  conta :");
+		int numero_O=SmartBank.teclado.nextInt();
+		
+		Conta conta_O = SmartBank.buscarContapornumero(numero_O);
+		
+		 if(conta_O!=null) {
+			 System.out.println("Informe o numero da  conta Destino :");
+				int numero_D=SmartBank.teclado.nextInt();
+				
+				Conta conta_D = SmartBank.buscarContapornumero(numero_D);
+				
+				 if(conta_D!=null) {
+					 System.out.println("Informe o valor de Tranferencia");
+					 Double val=SmartBank.teclado.nextDouble();
+					 conta_O.transferir(conta_D, val);
+				 }
+				 else {
+					 System.out.println(" Não foi encontrado a conta Destino numero "+numero_D);
+				 }
+			 	  
+		 }
+		 else {
+			 System.out.println(" Não foi encontrado a conta numero "+numero_O);
+		 }
+		 Utils.pausar(5);
+		 SmartBank.menu();
+		
+		
+	}
+
+	private static Conta buscarContapornumero(int numero) {
+		  Conta c= null;
+		   if(SmartBank.contas.size()>0) {
+			   for(Conta ct: SmartBank.contas) {
+				   if(ct.getNumero()==numero) {
+					   c=ct;
+				   }
+			   }
+		   }
+		return c;
+	}
+
+	private static void EfetuarDeposito() {
+		System.out.println("Efetuando Deposito ...");
+		
+		System.out.println("Informe o numero da conta :");
+		int numero=SmartBank.teclado.nextInt();
+		
+		Conta conta = SmartBank.buscarContapornumero(numero);
+		
+		 if(conta!=null) {
+			 System.out.println("Informe o valor de Deposito");
+			 Double val=SmartBank.teclado.nextDouble();
+			 conta.depositar(val);
+		 }
+		 else {
+			 System.out.println(" Não foi encontrado a conta numero "+numero);
+		 }
+		 Utils.pausar(5);
+		 SmartBank.menu();
+		
+	}
+
+	private static void EfetuarSaque() {
+		System.out.println("Efetuando Saque ...");
+		
+		System.out.println("Informe o numero da conta :");
+		int numero=SmartBank.teclado.nextInt();
+		
+		Conta conta = SmartBank.buscarContapornumero(numero);
+		 if(conta!=null) {
+			 System.out.println("Informe o valor do Saque");
+			 Double val=SmartBank.teclado.nextDouble();
+			 conta.Sacar(val);
+		 }
+		 else {
+			 System.out.println(" Não foi encontrado a conta numero "+numero);
+		 }
+		 Utils.pausar(5);
+		 SmartBank.menu();
+		
+	}
+
+	private static void CriarConta() {
+		System.out.println("Criando Conta ...");
+		
+		System.out.println("Informe dados Cliente: ");
+		
+		System.out.println("Nome: ");
+		String Nome=SmartBank.teclado.nextLine();
+		
+		System.out.println("Email: ");
+		String Email=SmartBank.teclado.nextLine();
+		
+		System.out.println("Cpf: ");
+		String Cpf=SmartBank.teclado.nextLine();
+		
+		System.out.println("Data Nascimento: ");
+		String DataNasc=SmartBank.teclado.nextLine();
+		
+		Cliente cliente=new Cliente(Nome,Email,Cpf,Utils.stringParaData(DataNasc));
+		
+		Conta conta= new Conta(cliente);
+		contas.add(conta);
+		System.out.println(" Conta Criada Com Sucesso! ");
+		System.out.println("Dado da Conta: ");
+		System.out.println(conta);
+		System.out.println();
+		
+		Utils.pausar(5);
+		SmartBank.menu();
+		
 	}
 
 }
